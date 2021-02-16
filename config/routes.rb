f968afc25aa2c6ea4e15d845f7ea6e12329e98bb
Sarah OGrady <sarahfrancesogrady@gmail.com>
2021-02-16 18:09:26 +0000

    done app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get '/pets', to: 'pets#index'
+  get '/pets/new', to: 'pets#new', as: :new_pet
+  get '/pets/:id', to: 'pets#show', as: :pet
+  post '/pets', to: 'pets#create'
+  get '/pets/:id/edit', to: 'pets#edit', as: :edit_pet
+  patch '/pets/:id', to: 'pets#update'
+  delete '/pets/:id', to: 'pets#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Pet.create(
+    name: Faker::Name.name,
+    species: %w(dragon koala whale monkey Emu Elephant Kangaroo Worm Axolotl).sample,
+    address: Faker::Address.street_name,
+    found_on: Faker::Date.between(from: 10.days.ago, to: Date.today)
+  )
+end
